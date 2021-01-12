640d6927e2f292c81615fd725ded4d86e06149cb
Ahmed-aed <aeid767@gmail.com>
2021-01-12 14:19:30 +0200

    add route 404 related #5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Favorite from './pages/Favorite';
 import Details from './pages/Details';
+import NotFound from './pages/404';
+
 
 function App() {
   return (
     <Router>
       <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/favorite">Favorite</Link>
+          </li>
+        </ul>
         <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/favorite" component={Favorite} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/favorite" component={Favorite} />
           <Route path="/movie/:id" component={Details} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </Router>
